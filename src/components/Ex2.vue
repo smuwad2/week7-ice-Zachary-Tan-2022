0ec019c7a77800953d31d2ccf577821b4488373b
Zachareee <zacharytanhx@yahoo.com>
2025-10-22 11:10:30 +0800

    Passing testcases

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,18 +1,34 @@
 <script>
-    export default { 
-        // Add code in appropriate places
-        data() {
-            return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
-            }
-            
-        }
+export default {
+  // Add code in appropriate places
+  data() {
+    return {
+      items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+      newItem: ""
     }
+  },
+  methods: {
+    addItem() {
+      this.items.push(this.newItem)
+      this.newItem = ""
+    },
+    removeItem(idx) {
+      this.items.splice(idx, 1)
+    }
+  }
+}
 </script>
 
 <template>
 
-    <h2>Shopping Cart</h2>
-    <!-- TODO: Add Code Here -->
-   
+  <h2>Shopping Cart</h2>
+  <!-- TODO: Add Code Here -->
+  <ul>
+    <li v-for="(item, idx) in items" :key="idx">
+      {{ item }}
+      <button @click="removeItem(idx)">Delete!</button>
+    </li>
+  </ul>
+  <input v-model="newItem" />
+  <button @click="addItem">Add!</button>
 </template>
--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,32 +1,52 @@
 <script>
-    export default { 
+import axios from 'axios';
 
-       // add code here
+export default {
 
+  // add code here
+  data() {
+    return {
+      subject: "",
+      entry: "",
+      mood: "Happy",
+      moods: ["Happy", "Sad", "Angry"]
     }
+  },
+
+  methods: {
+    createPost() {
+      const queryParams = new URLSearchParams({ subject: this.subject, entry: this.entry, mood: this.mood })
+      axios.get(`http://localhost:3000/addPost?${queryParams}`)
+      this.subject = ""
+      this.entry = ""
+      this.mood = ""
+    }
+  }
+}
 </script>
 
 <template>
-    <div class="table m-2">
-        <h3>Add a New Blog Post</h3>
+  <div class="table m-2">
+    <h3>Add a New Blog Post</h3>
 
-        Subject: <input type='text' size='30' v-model='subject' required>
-        <br>
+    Subject: <input type='text' size='30' v-model='subject' required>
+    <br>
 
-        Entry: <br>
-        <textarea name='entry' cols='80' rows='5' v-model='entry' required></textarea>
-        <br>
+    Entry: <br>
+    <textarea name='entry' cols='80' rows='5' v-model='entry' required></textarea>
+    <br>
 
-        Mood:
-        <!-- TODO: Build a dropdown list here for selecting the mood -->
+    Mood:
+    <select role="combobox" v-model="mood">
+      <option v-for="(mood, idx) in moods" :key="idx" :value="mood">{{ mood }}</option>
+    </select>
 
-        <br>
+    <br>
 
-        <br>
-        <button>Submit New Post</button>
+    <br>
+    <button @click="createPost">Submit New Post</button>
 
-        <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
-       
-    </div>
+    <hr> Click <a><router-link to="/ViewPosts/">here</router-link></a> to return to Main Page
+
+  </div>
 </template>
-
